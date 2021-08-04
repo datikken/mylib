4f51710a60b709070968fe5ef4250f14039d9b8a
datikken <tikken23@gmail.com>
2021-08-04 17:05:51 +0300

    'models';

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TopPageModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
