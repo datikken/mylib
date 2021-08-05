25605b5eb28c45fd89bfbfa508b00140d5f15404
datikken <tikken23@gmail.com>
2021-08-05 19:09:27 +0300

    validation pipes;

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,14 +1,34 @@
-import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
-import {ReviewModel} from "./review.model";
+import {Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UsePipes, ValidationPipe} from '@nestjs/common';
+import {CreateReviewDTO} from './dto/create-review.dto';
+import {ReviewService} from './review.service';
+import {REVIEW_NOT_FOUND} from './review.constants';
+
 
 @Controller('review')
 export class ReviewController {
+	constructor(private readonly reviewService: ReviewService) {}
+
+	@UsePipes(new ValidationPipe())
 	@Post('create')
-	async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
+	async create(@Body() dto: CreateReviewDTO) {
+		return this.reviewService.create(dto);
+	}
+
+	@Get('/')
+	async getAllReviews() {
+		return this.reviewService.getAllReviews();
+	}
 
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {}
+	async getByProduct(@Param('productId') productId: string) {
+		return this.reviewService.findByProductId(productId);
+	}
 
 	@Delete(':id')
-	async delete(@Param('id') id: string) {}
+	async delete(@Param('id') id: string) {
+		const deletedDoc = await this.reviewService.delete(id);
+		if(deletedDoc) {
+			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
+		}
+	}
 }
